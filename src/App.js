05573d2b85951617177720fc00884952e51279b4
Jsubak <tnwjd1003@gmail.com>
2021-09-16 23:30:01 +0900

    Edit javascript
    수업 내용대로 자바스크립트 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react"
+const foodLike = [
+  {
+    id: 1,
+    name : "chikin",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGbP0Y0vXZBdo7p4sYAKh7fm0lAsflEkAZA&usqp=CAU"
+  },
+  {
+    id: 2,
+    name : "pizza",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBiAL4n_yvKZR3guA_h0wNiNcpd39irG0Tuw&usqp=CAU"
+  }
+]
 
 function App() {
   return (
     <div>
-      Hello React!!!!
+      {
+        foodLike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))
+      }
     </div>
+  )
+}
+
+function Food({name, picture}) {
+  return (
+  <div>
+    <h1>I like {name} </h1>
+    <img src={picture} />  
+  </div>
   )
 }
 
