e1ab9288476fa3e585eae5af9a48bd5750dec448
Jsubak <tnwjd1003@gmail.com>
2021-10-01 22:56:26 +0900

    Edit JS
    현재 사용하는 자바스크립트

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-const foodLike = [
-  {
-    id: 1,
-    name : "chikin",
-    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGbP0Y0vXZBdo7p4sYAKh7fm0lAsflEkAZA&usqp=CAU"
-  },
-  {
-    id: 2,
-    name : "pizza",
-    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBiAL4n_yvKZR3guA_h0wNiNcpd39irG0Tuw&usqp=CAU"
-  }
-]
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      {
-        foodLike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))
-      }
-    </div>
-  )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log('constructor')
+    }
+
+    componentDidCMount() {
+      console.log('componentDidMount')
+    }
+
+    componentDidUpdate() {
+      console.log("componentDidUpdate")
+    }
+
+    state = {
+        count: 0
+    }
+
+    add = () => {
+      this.setState({count: this.state.count + 1})
+    }
+
+    minus = () => {
+      this.setState({count: this.state.count - 1})
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>The number is: {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
+        </div>
+      )
+    }
 }
 
-function Food({name, picture}) {
-  return (
-  <div>
-    <h1>I like {name} </h1>
-    <img src={picture} />  
-  </div>
-  )
-}
+
 
 export default App
